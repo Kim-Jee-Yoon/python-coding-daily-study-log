a87dbd074c7b1de6c24881e4e8d9830d87d968b7
Kim-Jee-Yoon <mstella991010@gmail.com>
2025-06-30 01:31:20 +0900

    Update day3 문제.py

--- day3 문제.py
+++ day3 문제.py
@@ -13,44 +13,8 @@
 
 #한 줄에 하나씩 출력
 
-#N = input("숫자를 입력해주세요")
-#n = int(N)
 
-#result = []
-#for i in range (1, n+1):
-    #keyNum = i % 3== 0 or i % 5== 0
-    #if keyNum == (i % 3 ==0 and i % 5==0):
-        #pass
-    #result.append(keyNum)
-
-#print(result)
-
-#결과가 [False, False, True...] 이런 식으로 나와서 다시 풀기
-##keyNum이 True, False로 나와서 문제임.
-#N = input("숫자를 입력해주세요")
-#n = int(N)
-
-#result = []
-#for i in range (1, n+1):
-    #if  i % 3== 0 or i % 7== 0:
-        #result.append(i)
-        #if i % 3 ==0 and i % 7==0:
-            #result.remove(i)
-    
-#print(result)
-
-#%%문제 잘못 읽었네.. 한 줄 씩 출력해야지. 문제 파악부터 제대로/ 리스트를 빼자.
-N = input("숫자를 입력해주세요")
-n = int(N)
-
-for i in range (1, n+1):
-    if  i % 3== 0 or i % 7== 0:
-        if i % 3 ==0 and i % 7==0:
-            continue
-        print(i)
-        
-
-#%%개선한 답
+ 
 n1 = int(input("숫자를 입력하세요"))
 
 for x in range(1, n1+1):
